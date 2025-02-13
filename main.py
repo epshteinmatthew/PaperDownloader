71b337ab17fad5f571125858356f21031ccd1003
epshteinmatthew <robotmeatsuit@gmail.com>
2025-02-12 16:59:34 -0800

    new papers

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #https://modeldb.science/api/v1/papers
 import urllib.request
 from bs4 import BeautifulSoup
-
+from pathlib import Path
+#from paperscraper.pdf import save_pdf
 import requests, json
 
 paperslist = requests.get("https://modeldb.science/api/v1/papers").json()
@@ -12,11 +13,17 @@ def downloadpaper(code, dest):
         pmid = requests.get("https://modeldb.science/api/v1/papers/" + paper).json()["pubmed_id"]["value"]
     except:
         return
+
+
     pmdata = requests.get("https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?tool=my_tool&email=my_email@example.com&ids=" + pmid + "&format=json").json()["records"][0]
     if("pmcid" not in pmdata.keys()):
         return
     pmcid = pmdata["pmcid"]
-    #403! need to get around this somehow
+
+    my_file = Path("papers/" + pmcid + ".pdf")
+    if my_file.is_file():
+        return
+
     #https://pmc.ncbi.nlm.nih.gov/tools/oai/
     fileadress = requests.get("https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi?id=" + pmcid + "&format=pdf", allow_redirects=True).content
     data = BeautifulSoup(fileadress, "xml")
@@ -31,4 +38,4 @@ def downloadpaper(code, dest):
 
 
 for paper in paperslist:
-    downloadpaper(paper, "papers")
+    downloadpaper(paper, "papers/")
